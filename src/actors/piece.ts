fb3e8da9953388040a5a4d815118154c0f335a67
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2022-04-18 20:10:49 -0500

    king

--- src/actors/piece.ts
+++ src/actors/piece.ts
@@ -1,6 +1,7 @@
 import { Actor, ImageSource, vec, Vector } from "excalibur";
 import { Move } from "../move";
 import { BishopRules } from "../pieceRules/bishopRules";
+import { KingRules } from "../pieceRules/kingRules";
 import { PawnRules } from "../pieceRules/pawnRules";
 import { QueenRules } from "../pieceRules/queenRules";
 import { RookRules } from "../pieceRules/rookRules";
@@ -83,6 +84,7 @@ export class Piece extends Actor {
             case "b": ruleObject = new BishopRules(); break;
             case "r": ruleObject = new RookRules(); break;
             case "q": ruleObject = new QueenRules(); break;
+            case "k": ruleObject = new KingRules(); break;
         }
 
         return ruleObject.getLegalMoves(board, pos);
---
+++ src/pieceRules/kingRules.ts
@@ -0,0 +1,104 @@
+import { vec, Vector } from "excalibur";
+import { Move } from "../move";
+import { Board } from "../scenes/board";
+import { PieceRule } from "./pieceRule";
+
+export class KingRules implements PieceRule {
+    getLegalMoves(board: Board, pos: Vector): Move[] {
+        
+        /**
+         * The King can move one space in every direction.
+         */
+
+        const moves: Move[] = [];
+
+        const ourType = board.getPieceType(pos);
+        const ourColor = board.getPieceColor(pos);
+        
+        // Temp variables for possible moves
+        let optionColor;
+        let optionPos;
+
+        // Moving one up
+        if (pos.y != 0) {
+            optionPos = pos.add(vec(0, -1));
+            optionColor = board.getPieceColor(optionPos);
+
+            if (optionColor != ourColor) {
+                moves.push(new Move(ourType, pos, optionPos));
+            }
+        }
+
+        // Moving one down
+        if (pos.y != 7) {
+            optionPos = pos.add(vec(0, 1));
+            optionColor = board.getPieceColor(optionPos);
+
+            if (optionColor != ourColor) {
+                moves.push(new Move(ourType, pos, optionPos));
+            }
+        }
+
+        // Moving one left
+        if (pos.x != 0) {
+            optionPos = pos.add(vec(-1, 0));
+            optionColor = board.getPieceColor(optionPos);
+
+            if (optionColor != ourColor) {
+                moves.push(new Move(ourType, pos, optionPos));
+            }
+        }
+
+        // Moving one right
+        if (pos.x != 7) {
+            optionPos = pos.add(vec(1, 0));
+            optionColor = board.getPieceColor(optionPos);
+
+            if (optionColor != ourColor) {
+                moves.push(new Move(ourType, pos, optionPos));
+            }
+        }
+
+        // Moving one up/left
+        if (pos.x != 0 && pos.y != 0) {
+            optionPos = pos.add(vec(-1, -1));
+            optionColor = board.getPieceColor(optionPos);
+
+            if (optionColor != ourColor) {
+                moves.push(new Move(ourType, pos, optionPos));
+            }
+        }
+
+        // Moving one up/right
+        if (pos.x != 7 && pos.y != 0) {
+            optionPos = pos.add(vec(1, -1));
+            optionColor = board.getPieceColor(optionPos);
+
+            if (optionColor != ourColor) {
+                moves.push(new Move(ourType, pos, optionPos));
+            }
+        }
+
+        // Moving one down/left
+        if (pos.x != 0 && pos.y != 7) {
+            optionPos = pos.add(vec(-1, 1));
+            optionColor = board.getPieceColor(optionPos);
+
+            if (optionColor != ourColor) {
+                moves.push(new Move(ourType, pos, optionPos));
+            }
+        }
+
+        // Moving one down/right
+        if (pos.x != 7 && pos.y != 7) {
+            optionPos = pos.add(vec(1, 1));
+            optionColor = board.getPieceColor(optionPos);
+
+            if (optionColor != ourColor) {
+                moves.push(new Move(ourType, pos, optionPos));
+            }
+        }
+
+        return moves;
+    }
+}
