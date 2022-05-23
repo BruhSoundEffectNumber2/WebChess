c130ec90713f45cc6da7a333e2c9b01d93444c4c
Damon <crazygamerdl@hotmail.com>
2022-05-23 10:20:42 -0700

    Worked more on screen sass

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import './styles/main.scss';
 import './styles/mainMenu.scss';
 import './styles/board.scss';
 
-import {Engine, Input, Loader} from 'excalibur';
+import {DisplayMode, Engine, Input, Loader, ScrollPreventionMode} from 'excalibur';
 import {ChessInput} from './input/chessInput';
 import {Resources} from './resources';
 import {Board} from './scenes/board';
@@ -16,17 +16,19 @@ export class Game extends Engine {
 
   constructor() {
     super({
-      width: 900,
-      height: 600,
       canvasElementId: 'game',
       pointerScope: Input.PointerScope.Document,
+      // Set the native resolution and allow CSS to set the canvas size
+      resolution: {width: 900, height: 600},
+      suppressPlayButton: true,
+      suppressConsoleBootMessage: true,
+      displayMode: DisplayMode.FitContainer,
     });
   }
 
   public override start() {
     // Automatically load all default resources
     const loader = new Loader(Object.values(Resources));
-    loader.suppressPlayButton = true;
 
     game.add('mainMenu', new MainMenu());
 
@@ -66,7 +68,6 @@ const game = Game.get();
 game.start().then(() => {
   game.goToScene('mainMenu');
 });
-
 
 game.input.pointers.primary.on('up', function (event) {
   // We should stop the input from proceeding if we are over a child of the ui
